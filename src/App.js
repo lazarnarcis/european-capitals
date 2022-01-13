b21cdb6f6177be7e835d721f18dcc755b9ef06aa
Lazar Narcis <lnarcis310@gmail.com>
2022-01-13 10:34:06 +0200

    new design and bug fixes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ export default function App() {
     const [errs, setErr] = useState("")
     const [score, setScore] = useState(localStorage.getItem('score') || '0')
     const [mistake, setMistake] = useState(localStorage.getItem('mistake') || '0')
+    const [errColor, setErrColor] = useState()
 
     useEffect(() => {
         localStorage.setItem('score', score)
         if (score < 0) {
             setErr("Scorul nu poate fi sub 0! Ti-au fost resetate toate punctele!")
+            setErrColor(2)
             setScore(0)
             setMistake(0)
         } else if (score >= 50) {
             setScore(0)
             setErr("Felicitari! Ai completat corect cu cele 50 de capitale! Scorul ti-a fost resetat.")
+            setErrColor(1)
         }
     }, [score])
 
@@ -27,6 +30,7 @@ export default function App() {
         localStorage.setItem('mistake', mistake)
         if (mistake === 3) {
             setErr("Ti-au fost scazute 2 puncte pentru ca ai acumulat 3 greseli!")
+            setErrColor(2)
             setScore(score => Number(score) - Number(2))
             setMistake(0)
         }
@@ -36,8 +40,9 @@ export default function App() {
         <div className="App">
             <h1>Capitale din Europa</h1>
             <ItemCountry item={countries[generateRandomCountry(countries.length)]} score={score} 
-            setScore={setScore} mistake={mistake} setMistake={setMistake} setErr={setErr} />
-            <p style={{backgroundColor: `${errs ? "black" : null}`, color: "white"}} className={`${errs ? "err" : ""}`}>{errs}</p>
+            setScore={setScore} mistake={mistake} setMistake={setMistake} setErr={setErr}
+            errColor={errColor} setErrColor={setErrColor} />
+            <p style={{backgroundColor: `${errs ? `${errColor === 1 ? "green" : "red"}` : null}`, color: "white"}} className={`${errs ? "err" : ""}`}>{errs}</p>
             <p>Scor: {score} {score === 1 ? "punct" : "puncte" }</p>
             <p>Pana acum ai {mistake} {mistake === 1 ? "greseala" : "greseli"}!</p>
         </div>
--- src/ItemCountry.js
+++ src/ItemCountry.js
@@ -1,15 +1,17 @@
 import React, { useRef } from "react"
 import "./ItemCountry.scss"
 
-export default function ItemCountry({ item, score, setScore, mistake, setMistake, setErr }) {
+export default function ItemCountry({ item, score, setScore, mistake, setMistake, setErr, setErrColor }) {
     const inputRef = useRef()
     const checkCapital = () => {
         if (item.capital.toLowerCase() === inputRef.current.value) {
-            setErr("Capitala este corecta!")
+            setErr(`Corect! Capitala ${item.country} este ${item.capital} (+1 pct)`)
+            setErrColor(1)
             setScore(Number(score) + Number(1))
         } else {
             setErr(`Capitala nu este corecta, capitala ${item.country} este ${item.capital}!`)
             setMistake(Number(mistake) + Number(1))
+            setErrColor(2)
         }
         inputRef.current.value = ""
     }
@@ -20,7 +22,7 @@ export default function ItemCountry({ item, score, setScore, mistake, setMistake
     }
     return (
         <>
-            <h4>Introdu capitala {item.country} mai jos:</h4>
+            <h4>Introdu capitala <i>{item.country}</i> mai jos:</h4>
             <div className="inputs">
                 <input type="text" ref={inputRef} onKeyDown={handleKeyDown} placeholder="Introdu capitala aici" />
                 <button onClick={checkCapital}>Verifica</button>
